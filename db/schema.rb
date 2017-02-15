99a8bcf955da92e7f8b7daeb63f7b2f92fda20c9
Anna <samslik@gmail.com>
2017-02-15 14:25:32 +0100

    4.2 fix color column in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214213630) do
+ActiveRecord::Schema.define(version: 20170215123309) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170214213630) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "colour"
+    t.string   "color"
     t.integer  "price"
   end
 
