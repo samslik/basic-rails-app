e847e2b017b77e1e21b306ab88f29d26ed1c3c8c
Anna <samslik@gmail.com>
2017-02-16 18:26:29 +0100

    add carousel with dynamic images to landing page, clean indexpage view with a partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,10 @@ class StaticPagesController < ApplicationController
   def index
   end
   def landing_page
-    @featured_product = Product.first
+    if Product.all.length < 4
+      @products = Product.limit(3)
+    else
+      @products = Product.limit(4)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  # root 'static_pages#index'
-    root 'static_pages#landing_page'
-
   resources :orders, only: [:index, :show, :create, :destroy]
+
+  # root 'static_pages#index'
+  root 'static_pages#landing_page'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
