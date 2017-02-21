510852345fa4abfe7ec94a09a2a12b6053889ff2
Anna <samslik@gmail.com>
2017-02-21 15:36:28 +0100

    5.1 add dynamic links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
@@ -7,7 +8,10 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # root 'static_pages#index'
-  root 'static_pages#landing_page'
+  # root 'static_pages#landing_page'
+  root 'products#index'
+
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
