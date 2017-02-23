95f5060ba66b8d5cc25728e0c169322da4a8f573
Anna <samslik@gmail.com>
2017-02-23 15:29:29 +0100

    5.3 add contatc form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
+  post 'static_pages/thank_you'
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # root 'static_pages#index'
