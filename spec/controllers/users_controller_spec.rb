07516c6ac3be3a0c86f4ffbe6788dfdbcea100ad
Anna <samslik@gmail.com>
2017-03-10 11:41:21 +0100

    6.3 correct syntax in functional tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,18 +11,18 @@ describe UsersController, :type => :controller do
         sign_in @user
       end
       it 'loads correct user details' do
-        get :show, id: @user.id
+        get :show, params: {id: @user.id}
         expect(response).to have_http_status(200)
         expect(assigns(:user)).to eq @user
       end
       it "can't access show page of the 2. user" do
-        get :show, id: @user2.id
+        get :show, params: {id: @user2.id}
         expect(response).to redirect_to(root_path)
       end
     end
     context 'No user is logged in' do
       it 'redirects to login' do
-        get :show, id: @user.id
+        get :show, params: {id: @user.id}
         expect(response).to redirect_to('/users/sign_in')
       end
     end
