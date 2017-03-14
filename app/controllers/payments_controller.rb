539677bcfa5b0a165e6884ae7c2e7496b097a78f
Anna <samslik@gmail.com>
2017-03-14 12:53:42 +0100

    6.6 make Stripe payments work

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,12 +1,12 @@
 class PaymentsController < ApplicationController
   def create
-    @product = params[:product_id]
+    @product = Product.find(params[:product_id])
     @user = current_user
     token = params[:stripeToken]
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-          :amount => (@product.price *100),
+          :amount => (@product.price * 100).to_i,
           :currency => "eur",
           :source => token,
           :description => params[:stripeEmail]
@@ -17,8 +17,6 @@ class PaymentsController < ApplicationController
           :user_id => @user,
           :total => @product.price
         )
-        # flash[:notice] = "Payment processed successfully"
-        # redirect_to product_path(@product)
       end
     rescue Stripe::CardError => e
       # The card has been declined
