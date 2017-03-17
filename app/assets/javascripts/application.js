8263a632605bae4e7c8fb00e9facc4882fefd93a
Anna <samslik@gmail.com>
2017-03-17 12:00:34 +0100

    6.9 add elevatezoom (javascript plugin)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,6 +30,8 @@ $(document).on('turbolinks:load', function(){
         }
     });
 
+    $('.img-zoom').elevateZoom();
+
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
         readOnly: true,
