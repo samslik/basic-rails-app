ee823ec4a28a027ed90745a7ea533a8d59be5188
Anna <samslik@gmail.com>
2017-03-28 17:10:18 +0200

    6.12

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module Nameofapp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
+    config.assets.initialize_on_precompile = false
   end
 end
 
