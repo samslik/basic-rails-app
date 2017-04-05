0c656526f6fe6f9e3c1e41782f8d024d40b627ce
Anna <samslik@gmail.com>
2017-04-05 13:20:49 +0200

    6.12 change redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
-  host = "redis://h:p58f5ee7ecfc4d469128c7907200ebd833d63b36606e7ec1206701241400d0239@ec2-34-206-56-5.compute-1.amazonaws.com:34009"
-  $redis = Redis.new(:host => host)
+  $redis = Redis.new(url: ENV["REDIS_URL"])
 else
   $redis = Redis.new(:host => 'localhost', :port => 6379)
 end
